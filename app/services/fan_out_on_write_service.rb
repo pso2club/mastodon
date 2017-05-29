47928b4958cf6de746053b50b740ba96a68cfc89
pso2club <pso2.club@pso2.club>
2017-05-29 11:51:34 +0900

    Fix: Union Timeline auto-refresh not working
    * fix(app/services/fan_out_on_write_service.rb) missing publish path

--- app/services/fan_out_on_write_service.rb
+++ app/services/fan_out_on_write_service.rb
@@ -68,6 +68,6 @@ class FanOutOnWriteService < BaseService
 
     Redis.current.publish('timeline:public', @payload)
     Redis.current.publish('timeline:public:local', @payload) if status.local?
-    Redis.current.publish('timeline:public:union', @payload) if status.union?
+    Redis.current.publish('timeline:union', @payload) if status.union?
   end
 end
